b7a6234f9cd9a8a191a8519d4b51a3fe6a4b88c6
Jørgen Nikolai Havnsund <Jorgen.havnsund@live.no>
2015-04-16 10:46:57 +0200

    Endringer i MediaReg og Media Class

--- Assignment 5 copy/Media.java
+++ Assignment 5 copy/Media.java
@@ -1,9 +1,11 @@
-
+//Forsøk på å importere Duration class
+//Husk å last ned nyaste Java JDK/SDK
+import java.util.*;
 /**
- * Write a description of class Media here.
+ * Creates a new media and adds it to a list.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Eirik Nesje) 
+ * @version (1.0)
  */
 public class Media
 {
@@ -11,28 +13,75 @@ public class Media
     private String title;
     private int durationMins;
     private int durationSecs;
+    private boolean startUpCount = true;
     private AudioTrack track;
+    private Media media;
+    private MediaReg medium;
+    //private Duration duration;
        
     /**
      * Constructor for objects of class Media
      */
     public Media()
     {
-        MediaGenetics();
+        MediaGenetics(title);
     }
     
     /**
-     * 
+     * Makes a new HashMap for all the media and adds the media to it.
      */
-    public void MediaGenetics()
-    {
-        
+    public void MediaGenetics(String title /*, Duration duration*/)
+    {   
+        this.title = title;
+        //this.duration = duration;
+        media = new Media();
+        //alternativ måte, få Audiotrack til å lage medium HashMap istedet.
+        if(startUpCount == true)
+        {
+            makeMediaList();
+            startUpCount = false;
+        }
+        medium.addMedia(title, media);
     }
     
+    /**
+     * Makes a new media list. Run this method only once.
+     */
+    public void makeMediaList()
+    {
+        medium = new MediaReg();
+    }
+    
+    /**
+     * @Return the media title.
+     */
     public String getTitle()
     {
         return title;
     }
-    //return duration
     
+    /**
+     * @Returns the duration of the media.
+     */
+    /*
+    public Duration getDuration()
+    {
+        return duration;
+    }
+    //return duration
+    */
+   /**
+    * Search the media and return either the media or gives an error.
+    * @Returns the media searched.
+    */
+   public void findMedia(String description)
+   {
+       //En sjekk i tilfelle nullpointer
+       if(media != null)
+       {
+           medium.getMedia(description);
+       }
+       else
+       System.out.println("There is no such media in the list");
+   }
 }
--- Assignment 5 copy/MediaReg.java
+++ Assignment 5 copy/MediaReg.java
@@ -1,16 +1,20 @@
 import java.util.HashMap;
+import java.util.Set;
 /**
- * Write a description of class MediaReg here.
+ * This class is a mediaregistry. This class is responsible for making 
+ * a new HashMap and some help functions to use it properly.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Eirik Nesje) 
+ * @version (1.0)
  */
 public class MediaReg
 {
     //Hashmap to keep all the Media in one place
+    private HashMap<String, Media> medium;
 
     /**
-     * Makes a HashMap with Media
+     * Constructor of MediaReg class. 
+     * @Runs MediaList().
      */
     public MediaReg()
     {
@@ -18,18 +22,58 @@ public class MediaReg
     }
 
     /**
-     * 
+     * Creates a new HashMap.
      */
     public void MediaList()
     {
         //make HashMap
+        medium = new HashMap<String, Media>();
     }
     
     /**
-     * 
+     * Runs a for each loop and gets all the HashMap keys and returns them.
+     * @Return the whole Medialist.
      */
-    public void GetMediaList()
+    public String GetMediaList()
     {
         //return MediaList();
+        String returnString = " ";
+        Set<String> keys = medium.keySet();
+        for(String Media : keys)
+        {
+            returnString += " " + Media;
+        }
+        return returnString;
+    }
+    
+    /**
+     * @Returns the whole HashMap list.
+     */
+    public HashMap<String, Media> HashMap()
+    {
+        return medium;
+    }
+    
+    /**
+     * Adds a media to the HashMap media list.
+     */
+    public void addMedia(String description, Media media)
+    {
+        medium.put(description, media);
+    }
+    
+    /**
+     * Removes a media from the HashMap media list.
+     * @Returns the media removed.
+     */
+    public Media removeMedia(String description)
+    {
+        return medium.remove(description);
+    }
+    
+    public Media getMedia(String description)
+    {
+        Media media = medium.get(description);
+        return media;
     }
 }
